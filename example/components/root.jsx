f23d733fd6cdcaa528904a06b6a1b6ae452d6d71
Phil Barow <pbarow@gmail.com>
2017-06-28 16:57:17 -0400

    hot reloading working without making manh files

--- example/components/root.jsx
+++ example/components/root.jsx
@@ -6,8 +6,8 @@ const Root = () => (
   <div>
     <div className="beach">Hello React Hot Loader!</div>
     <h2>Adding some more code here</h2>
-    <h6>and here !!!</h6>
+    <h6>and here</h6>
   </div>
     )
 
-export default Root
+export default Root;
